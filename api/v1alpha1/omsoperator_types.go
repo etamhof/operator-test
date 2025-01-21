86e3124e2e3d6b185990e79ab221253f8c6052b6
etamhof <tamas.ferenc.hoffman@ericsson.com>
2025-01-21 17:42:26 +0100

    Test update

--- api/v1alpha1/omsoperator_types.go
+++ api/v1alpha1/omsoperator_types.go
@@ -29,6 +29,7 @@ type OmsOperatorSpec struct {
 	// Important: Run "make" to regenerate code after modifying this file
 
 	EndPoint int `json:"endPoint,omitempty"`
+	Useless  int `json:"useless,omitempty"`
 }
 
 // OmsOperatorStatus defines the observed state of OmsOperator
